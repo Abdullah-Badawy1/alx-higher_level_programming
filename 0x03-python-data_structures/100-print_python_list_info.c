d51d7546831c99d1e90020a011faa14a314d4362
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-11-06 14:01:51 -0800

    mody

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,19 +1,15 @@
 #include <Python.h>
+#include <object.h>
+#include <listobject.h>
 
 void print_python_list_info(PyObject *p)
 {
-    long int list_size = PyList_Size(p);
-    int index = 0;
-    PyListObject *list_obj = (PyListObject *)p;
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *obj = (PyListObject *)p;
 
-    printf("[*] Size of the Python List = %li\n", list_size);
-
-    // Avoid displaying 'allocated' to prevent potential cheating.
-    
-    while (index < list_size)
-    {
-        PyObject *item = PyList_GetItem(p, index);
-        printf("Element %i: %s\n", index, Py_TYPE(item)->tp_name);
-        index++;
-    }
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 }
