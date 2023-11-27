7134e19a1ace5d8fc3766b09880727ff6be4dccf
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-11-27 04:50:02 -0800

    enhance the fourth file

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,124 +1,113 @@
 #!/usr/bin/python3
-"""Module 3-rectangle for Holberton Python project 0x08 task 3.
-
-This module defines a Rectangle class with functionalities for calculating
-area and perimeter, and includes a method to produce a string representation
-of the rectangle.
-
+"""4-rectangle, built for Holberton Python project 0x08 task 4.
 """
 
 
 class Rectangle:
-    """A class for representing a rectangle.
+    """Takes in args for width and height of a rectangle, and contains methods
+    for calculation of the area or perimeter.
 
-    This class provides the functionality to create a rectangle object with
-    width and height attributes. It includes methods to calculate the area
-    and perimeter of the rectangle and a special method to output a string
-    representation of the rectangle using the `__str__` method.
+    __str__ and __repr__ fuctionality defined below.
 
     Args:
-        width (int, optional): The width of the rectangle. Defaults to 0.
-        height (int, optional): The height of the rectangle. Defaults to 0.
+        width (int): horizontal dimension of rectangle, defaults to 0
+        height (int): vertical dimension of rectangle, defaults to 0
 
     """
-
     def __init__(self, width=0, height=0):
-        """Initializes a new Rectangle instance.
-
-        The initialization sets the width and height of the rectangle using
-        property setters which include validations for type and value.
-
-        Args:
-            width (int): The initial width of the rectangle.
-            height (int): The initial height of the rectangle.
-
-        """
-        self.width = width  # Set width using the width property setter
-        self.height = height  # Set height using the height property setter
+        # attribute assigment here engages setters defined below
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """int: Gets or sets the width of the rectangle.
+        """__width getter.
 
-        The setter ensures the width is an integer and not less than 0.
-
-        Raises:
-            TypeError: If `value` is not an integer.
-            ValueError: If `value` is less than 0.
+        Returns:
+            __width (int): horizontal dimension of rectangle
 
         """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Sets the width of the rectangle.
+        """Args:
+            value (int): horizontal dimension of rectangle
 
-        Validates that the width is an integer and not less than 0.
+        Attributes:
+            __width (int): horizontal dimension of rectangle
 
-        Args:
-            value (int): The width of the rectangle.
+        Raises:
+            TypeError: If `value` is not an int.
+            ValueError: If `value` is less than 0.
 
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError('width must be an integer')
-        if value < 0:
+        elif value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
     def height(self):
-        """int: Gets or sets the height of the rectangle.
+        """__height getter.
 
-        The setter ensures the height is an integer and not less than 0.
-
-        Raises:
-            TypeError: If `value` is not an integer.
-            ValueError: If `value` is less than 0.
+        Returns:
+            __height (int): vertical dimension of rectangle
 
         """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Sets the height of the rectangle.
+        """Args:
+            value (int): vertical dimension of rectangle
 
-        Validates that the height is an integer and not less than 0.
+        Attributes:
+            __height (int): vertical dimension of rectangle
 
-        Args:
-            value (int): The height of the rectangle.
+        Raises:
+            TypeError: If `value` is not an int.
+            ValueError: If `value` is less than 0.
 
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
 
     def area(self):
-        """Calculates the area of the rectangle.
+        """Returns area of a rectangle of a given `width` and `height`.
 
-        Computes the area as the product of width and height.
+        Attributes:
+            __width (int): horizontal dimension of rectangle
+            __height (int): vertical dimension of rectangle
 
         Returns:
-            int: The area of the rectangle.
+            Area of rectangle: __width * __height
 
         """
         return self.__width * self.__height
 
     def perimeter(self):
-        """Calculates the perimeter of the rectangle.
+        """Returns the perimeter of a rectangle of given `width` and `height`
 
-        Computes the perimeter as twice the sum of width and height.
-        Returns 0 if either width or height is 0.
+        Attributes:
+            __width (int): horizontal dimension of rectangle
+            __height (int): vertical dimension of rectangle
 
         Returns:
-            int: The perimeter of the rectangle, or 0 if width or height is 0.
+            0 if either attribute is 0, or the perimeter: (__width * 2) +
+            (__height * 2).
 
         """
-        if self.__width == 0 or self.__height == 0:
+        if self.__width is 0 or self.__height is 0:
             return 0
-        return (self.__width * 2) + (self.__height * 2)
- def _draw_rectangle(self):
+        else:
+            return (self.__width * 2) + (self.__height * 2)
+
+    def _draw_rectangle(self):
         """Formats a string of '#' and '\n' chars to print the rectangle
         represented by the current instance.
 
