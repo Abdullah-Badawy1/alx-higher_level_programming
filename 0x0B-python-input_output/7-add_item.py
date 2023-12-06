11475b39b8d646b676d2b66ec194448ed87e8c42
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-12-05 16:16:21 -0800

    add task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-from json import dumps
+"""Module containing function to save python object in json format"""
+import json
 
 
 def save_to_json_file(my_obj, filename):
+    """Convert `my_obj` to json string and save to `filename`
+
+    Args:
+        my_obj: serializable object to convert to json
+        filename (str): file to save json string to
+    """
     with open(filename, 'w') as f:
-        a = dumps(my_obj)
-        f.write(a)
+        json.dump(my_obj, f)
